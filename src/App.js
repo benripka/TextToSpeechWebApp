ffed0fbde5ba19a650f32e863a25b8f5e60e57e5
benripka <benripka@gmail.com>
2019-12-31 04:13:14 -0700

    Added amazon polly and cognito, and got the text to speach up and running for small text uploads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useReducer } from 'react';
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports"; 
+import { withAuthenticator} from "aws-amplify-react";
 import './App.css';
 import {InitialState, Reducer} from "./Reducer";
 import {NavBar} from "./Components/NavBar";
 import {Footer} from "./Components/Footer";
 import {Body} from "./Components/Body";
 import { Grid } from '@material-ui/core';
+
+Amplify.configure(awsconfig); 
+
 
 
 function App() {
@@ -31,4 +37,45 @@ function App() {
   );
 }
 
-export default App;
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'My user name',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+    {
+      label: 'PhoneNumber',
+      key: 'phone_number',
+      required: true,
+      displayOrder: 3,
+      type: 'string'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 4,
+      type: 'string'
+    }
+  ]
+};
+const usernameAttributes = 'My user name';
+
+export default withAuthenticator(App, {
+  signUpConfig,
+  usernameAttributes
+});
+
--- src/Components/PdfReader.jsx
+++ src/Components/PdfReader.jsx
@@ -1,5 +1,10 @@
-import React, { useState } from "react";
-import { Stepper, Step, StepLabel, StepContent, Button, Paper, FormControl, RadioGroup, FormControlLabel, Radio} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Stepper, Step, StepLabel, StepContent, Button, Paper, FormControl, RadioGroup, FormControlLabel, Radio, TextField} from "@material-ui/core";
+import AWS from "aws-sdk";
+import Sound from "react-sound";
+
+const  tryPolly = (text, updateAudio) => {
+}
 
 const FileFormatSelector = (props) => {
 
@@ -41,12 +46,110 @@ const FileFormatSelector = (props) => {
     )
 }
 
+const InputTextForTranslation = () => {
+
+    const [value, changeValue] = useState("");
+    const [isLoading, changeIsLoading] = useState(true);
+    const [playing, changePlaying] = useState("STOPPED");
+    const [audio, changeAudio] = useState(null);
+
+    useEffect(() => {
+        console.log(audio);
+        if(audio !== null && audio !== undefined) {
+            changePlaying("PLAYING");
+            changeIsLoading(false);
+            console.log("Playing!")
+            console.log(isLoading)
+            console.log(audio);
+        }
+    }, [audio]);
+
+    const handleChange = (event) => {
+        changeValue(event.target.value);
+    }
+
+    const handleClick = (event) => {
+        
+        // Set the region where your identity pool exists (us-east-1, eu-west-1)
+        AWS.config.region = 'us-east-1';
+
+        // Configure the credentials provider to use your identity pool
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:0e8a0974-f61c-4030-af4c-b2a6f0f71fca',
+            Logins: { // optional tokens, used for authenticated login
+
+            }
+        });
+
+        // Make the call to obtain credentials
+        AWS.config.credentials.get(function(){
+
+            // Credentials will be available when this function is called.
+            var accessKeyId = AWS.config.credentials.accessKeyId;
+            var secretAccessKey = AWS.config.credentials.secretAccessKey;
+            var sessionToken = AWS.config.credentials.sessionToken;
+
+        });
+
+        var params = { 
+            OutputFormat: "mp3", 
+            SampleRate: "8000", 
+            Text: value, 
+            TextType: "text", 
+            VoiceId: "Joanna"
+        };
+
+        var polly = new AWS.Polly();
+
+        polly.synthesizeSpeech(params, (error, data) => {
+
+            if(error) console.log("Error from polly: ", error);
+            console.log(data);
+
+            var uint8Array = new Uint8Array(data.AudioStream);
+            var arrayBuffer = uint8Array.buffer;
+            var blob = new Blob([arrayBuffer]);
+
+            var url = URL.createObjectURL(blob);
+
+            console.log(url)
+
+
+            changeAudio(url);
+        })
+    }
+
+    return (
+        <div>
+            <Paper>
+            <form noValidate autoComplete="off">
+                <TextField
+                    id="text-input"
+                    lable="Text to Vocalize"
+                    multiline
+                    rows="10"
+                    value={value}
+                    onChange={handleChange}
+                />
+            </form>
+            <Button variant="contained" color="primary" onClick={handleClick}>
+                Read Out Loud
+            </Button>
+            </Paper>
+            <Sound 
+                url={audio}
+                playStatus={playing}
+            />
+            </div>
+    )
+}
+
 const getStepContent = (index) => {
     switch (index) {
         case 0:
             return <FileFormatSelector />
         case 1: 
-            return <h1>Second Step Content</h1>
+            return <InputTextForTranslation /> 
         case 2:
             return <h1>Third Step Content</h1>
         default:
